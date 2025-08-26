b1ad63f278f6094dd746987364eb55d1213667d6
Renan <renan_cesar2000@hotmail.com>
2025-08-25 21:11:07 -0300

    add unity diverseties and value promo per unity/new functions

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,26 +1,48 @@
-let handleInputPrice = document.querySelector("#price_of_oil");
-let showValue = document.querySelector("#showValue");
-let showFifteenPercent = document.querySelector("#showFifteenPercent");
-let showEighteenPercent = document.querySelector("#showEighteenPercent");
-let btnCalculate = document.querySelector("#btnCalculator");
-let btnReset = document.querySelector("#btnReset");
+const handleInputPrice = document.querySelector("#price_of_oil");
+const showValue = document.querySelector("#showValue");
+const showFifteenPercent = document.querySelector("#showFifteenPercent");
+const showEighteenPercent = document.querySelector("#showEighteenPercent");
+const btnCalculate = document.querySelector("#btnCalculator");
+const btnReset = document.querySelector("#btnReset");
+const literPromo15 = document.querySelector("#literPromo15");
+const literPromo18 = document.querySelector("#literPromo18");
+const boxQuantity = document.querySelector("#box_quantity");
 
 let boxTotal;
 let firstDiscount;
 let secondDiscount;
+let firstDiscountPromo;
+let secondDiscountPromo;
+let selectValue = boxQuantity.value;
 
-function calculate(oilValue) {
-    boxTotal = oilValue * 24;
+function calculate(oilValue, un) {
+    boxTotal = oilValue * un;
     firstDiscount = boxTotal / 100 * 85;
     secondDiscount = boxTotal / 100 * 82;
     return
 }
+function calculatePromo(unPromo) {
+    firstDiscountPromo = firstDiscount / unPromo;
+    secondDiscountPromo = secondDiscount / unPromo;
+}
+
+boxQuantity.addEventListener("change", (e) => {
+    selectValue = e.target.value;
+})
 
 btnCalculate.addEventListener("click", () => {
-    calculate(handleInputPrice.value);
+    if (selectValue == 24) {
+        calculate(handleInputPrice.value, selectValue);
+        calculatePromo(selectValue);
+    } else {
+        calculate(handleInputPrice.value, selectValue);
+        calculatePromo(selectValue);
+    }
     showValue.innerHTML = `R$${boxTotal.toFixed(2)}`;
     showFifteenPercent.innerHTML = `R$${firstDiscount.toFixed(2)}`;
+    literPromo15.innerHTML = `Litro à R$${firstDiscountPromo.toFixed(2)}`;
     showEighteenPercent.innerHTML = `R$${secondDiscount.toFixed(2)}`;
+    literPromo18.innerHTML = `Litro à R$${secondDiscountPromo.toFixed(2)}`;
 });
 
 btnReset.addEventListener("click", () => {
